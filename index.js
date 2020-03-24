97bcc6c62f97aa1a33002f0ac1145be54b51d9d7
Awais Akram Mughal <iamawaisakram@gmail.com>
2020-03-24 15:53:27 +0500

    Removed Console

--- index.js
+++ index.js
@@ -71,7 +71,6 @@ var unifiedServer = function(req, res) {
         : handlers.notFound;
 
     // * Construct the data object to send to the handler
-    console.log('called.....', buffer, helpers.parseJsonToObject(buffer));
     var data = {
       trimmedPath: trimmedPath,
       queryStringObject: queryStringObject,
